7be8be073cf4f8b731883d36fde00148a9c4a19a
COMP3100 SANGGON <sang-gon.jung@students.mq.edu.au>
2023-03-18 17:29:28 +1100

    week3 workshop

--- week3/MyClient.java
+++ week3/MyClient.java
@@ -1,15 +1,21 @@
-import java.io.*;  
 import java.net.*;  
-public class MyClient {  
-public static void main(String[] args) {  
-try{      
-Socket s=new Socket("localhost",6666);  
+import java.io.*;  
+class MyClient2{  
+public static void main(String args[])throws Exception{  
+Socket s=new Socket("localhost",3333);  
+DataInputStream din=new DataInputStream(s.getInputStream());  
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-dout.writeUTF("HELO");  
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+  
+String str="",str2="";  
+while(!str.equals("BYE")){  
+str=br.readLine();  
+dout.writeUTF(str);  
 dout.flush();  
+str2=din.readUTF();  
+System.out.println("Server says: "+str2);  
+}  
+  
 dout.close();  
 s.close();  
-}catch(Exception e){System.out.println(e);
-}  
-}
-}
+}}  
--- week3/MyServer.java
+++ week3/MyServer.java
@@ -1,14 +1,22 @@
-import java.io.*;  
 import java.net.*;  
-public class MyServer {  
-public static void main(String[] args){  
-try{  
-ServerSocket ss=new ServerSocket(6666);  
-Socket s=ss.accept();//establishes connection   
-DataInputStream dis=new DataInputStream(s.getInputStream());  
-String  str=(String)dis.readUTF();  
-System.out.println("message= "+str);  
+import java.io.*;  
+class MyServer2{  
+public static void main(String args[])throws Exception{  
+ServerSocket ss=new ServerSocket(3333);  
+Socket s=ss.accept();  
+DataInputStream din=new DataInputStream(s.getInputStream());  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+  
+String str="",str2="";  
+while(!str.equals("BYE")){  
+str=din.readUTF();  
+System.out.println("client says: "+str);  
+str2=br.readLine();  
+dout.writeUTF(str2);  
+dout.flush();  
+}  
+din.close();  
+s.close();  
 ss.close();  
-}catch(Exception e){System.out.println(e);}  
-}  
-}  
+}}  
